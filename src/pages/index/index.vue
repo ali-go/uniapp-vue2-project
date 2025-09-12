ba31e2d8c72f424116143a531857b0b7ffd64846
阿离 <1161653413@qq.com>
2025-09-12 11:26:49 +0800

    测试冲突

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,7 +2,7 @@
 	<view class="content">
 		<image class="logo" src="/static/logo.png"></image>
 		<view>
-			<text class="title">{{ title }}1111</text>
+			<text class="title">{{ title }}87984565</text>
 		</view>
 	</view>
 </template>
