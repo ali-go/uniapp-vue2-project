eb2595b25a9d96057734ee7483e1424e436546d1
[gameorstudy] <[jianyuxiong1998@gmail.com]>
2025-09-12 14:50:08 +0800

    feat: add eslint

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <script>
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-			console.log('env', process.env.NODE_ENV)
-			console.log('url', process.env.VUE_APP_BASE_URL)
-			console.log('url', process.env)
-		},
-		onShow: function() {
-			console.log('App Show1')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
-	}
+export default {
+  onLaunch: function () {
+    console.log('App Launch')
+    console.log('env', process.env.NODE_ENV)
+    console.log('url', process.env.VUE_APP_BASE_URL)
+  },
+  onShow: function () {
+    console.log('App Show1')
+  },
+  onHide: function () {
+    console.log('App Hide')
+  }
+}
 </script>
 
 <style>
-	/*每个页面公共css */
+/*每个页面公共css */
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,50 +1,46 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{ hello }}</text>
-		</view>
-	</view>
+  <view class="content">
+    <image class="logo" src="/static/logo.png"></image>
+    <view>
+      <text class="title">{{ hello }}</text>
+    </view>
+  </view>
 </template>
 
 <script>
 export default {
-	data() {
-		return {
-			hello: 'Hello'
-		}
-	},
-	onLoad() {
-
-	},
-	methods: {
-
-	}
+  data() {
+    return {
+      hello: 'Hello'
+    }
+  },
+  onLoad() {},
+  methods: {}
 }
 </script>
 
 <style lang="less">
 .content {
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-		text-decoration: underline;
-	}
+  .title {
+    font-size: 36rpx;
+    color: #8f8f94;
+    text-decoration: underline;
+  }
 }
 
 .logo {
-	height: 200rpx;
-	width: 200rpx;
-	margin: 200rpx auto 50rpx auto;
+  height: 200rpx;
+  width: 200rpx;
+  margin: 200rpx auto 50rpx auto;
 }
 
 .text-area {
-	display: flex;
-	justify-content: center;
+  display: flex;
+  justify-content: center;
 }
 </style>
