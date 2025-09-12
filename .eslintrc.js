e0a44fa97d156d6d5dfc8f9fcde3605d0ae91148
阿离 <1161653413@qq.com>
2025-09-12 16:54:53 +0800

    fix: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': 'off' // 或者 "error" 根据你的需求设置，如果你想要在保存时同时运行 ESLint 和 Prettier。
   }
 }
